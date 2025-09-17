79a0e32c5ef28c91bd058ea165587bc18d18a68d
Pratisthachand <pratisthachand22@gmail.com>
2025-09-16 22:31:43 -0500

    Initial commit

---
+++ add_one/app.py
@@ -0,0 +1,52 @@
+import json
+import time
+
+print("Initializing the lambda function")
+
+time.sleep(1) #simulating time taken to complete a large task
+
+print("Initialization complete!")
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+    try:
+        body = json.loads(event.get('body','{}'))
+        number = body.get('number',0)
+
+        result = number + 1
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": f"The result is {result}",
+            }),
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": f"An error occurred",
+                "error": str(e),
+            }),
+        }
+
+    
